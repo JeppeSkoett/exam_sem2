c85255ac326de2ae4ed31765c869f81efa2d3b56
JeppeSkoett <47398663+JeppeSkoett@users.noreply.github.com>
2019-05-21 23:13:45 +0200

    placering uden anchor points

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,7 @@
 function Scroll(){
     let top = document.getElementById('texttilkort')
     let ypos = window.pageYOffset;
-    if(ypos > 300){
+    if(ypos > 400){
        top.style.height = "470px";
        top.style.width = "400px"
        }
@@ -11,3 +11,4 @@ function Scroll(){
        }
 }
        window.addEventListener("scroll",Scroll);
+

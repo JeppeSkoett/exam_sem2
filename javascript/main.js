e3a0f61736a5c2173f19c45732b0cc8420834a24
JeppeSkoett <47398663+JeppeSkoett@users.noreply.github.com>
2019-05-21 13:42:56 +0200

    Javascript text til kort

---
+++ javascript/main.js
@@ -0,0 +1,13 @@
+function Scroll(){
+    let top = document.getElementById('texttilkort')
+    let ypos = window.pageYOffset;
+    if(ypos > 300){
+       top.style.height = "470px";
+       top.style.width = "400px"
+       }
+       else{ 
+       top.style.height = "0px";
+       top.style.width = "0px";
+       }
+}
+       window.addEventListener("scroll",Scroll);
